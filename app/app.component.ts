c5e5976f94ea6232b6c1263afd171be7372b7bb1
Christopher Ford <chris.ford@oneserve.co.uk>
2016-06-13 15:53:10 +0100

    Made some adjustments to the setup of the npm scripts for workflow

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<h1>Angular Skeleton</h1>
-  			<h3>Let's make something amazing</h3>`
+            <h3>Let's make something amazing</h3>`
 })
 
 export class AppComponent { }
